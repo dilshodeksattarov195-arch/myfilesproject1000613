937fb7503c4d398d969101604f406ded37156edf
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-26 16:29:11 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class paymentCeleteWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = paymentCeleteWorker()
+    print("Worker engine initialized.")
